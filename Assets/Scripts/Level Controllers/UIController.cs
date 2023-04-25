eaaba529610c50460841af150ec9bb0d7d91a76d
BRANDON MOONEY <2100664@uad.ac.uk>
2023-04-25 15:18:43 +0100

    fixing mushroom collision bug

--- Assets/Scripts/Level Controllers/UIController.cs
+++ Assets/Scripts/Level Controllers/UIController.cs
@@ -6,19 +6,23 @@ using TMPro;
 public class UIController : MonoBehaviour
 {
 
-   //Timer code from https://www.youtube.com/watch?v=hxpUk0qiRGs
+   //Timer and unit conversion code from https://www.youtube.com/watch?v=hxpUk0qiRGs
 
     
     [SerializeField] private float countdownTime = 180f;
+    private float countdownTimeAtStart;
     public TextMeshProUGUI timer;
     public bool countdownActive;
     [SerializeField] private LevelScript levelScript;
+    public bool runningOutOfTime;
     
     // Start is called before the first frame update
     void Start()
     {
 
         countdownActive = true;
+        runningOutOfTime = false;
+        countdownTimeAtStart = countdownTime;
     }
 
     // Update is called once per frame
@@ -38,6 +42,11 @@ public class UIController : MonoBehaviour
 
                 levelScript.GetComponent<LevelScript>().TimeUp();
             }
+            
+            if (countdownTime < countdownTimeAtStart / 6) 
+            {
+                runningOutOfTime = true;
+            }
         }
     
     }
@@ -50,6 +59,17 @@ public class UIController : MonoBehaviour
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
         timer.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        //changes text colour to red when timer hits under a 6th of its starting value (3 minutes -> 30 seconds | 1 minute -> 10 seconds)
+        if ( runningOutOfTime == true)
+        {
+            timer.color = Color.red;
+        }
+        else
+        {
+            timer.color = Color.white;
+        }
     }
+   
 
 }
