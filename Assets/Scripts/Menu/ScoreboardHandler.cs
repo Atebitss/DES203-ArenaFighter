41a9bcddea19ce0c8c47ea4e5e1ae2d1082454b5
Atebitss <joseph.c.1998@hotmail.co.uk>
2023-06-26 23:34:37 +0100

    scoreboard less players

--- Assets/Scripts/Menu/ScoreboardHandler.cs
+++ Assets/Scripts/Menu/ScoreboardHandler.cs
@@ -9,18 +9,34 @@ public class ScoreboardHandler : MonoBehaviour
     private float[] playerTSLKs = new float[PlayerData.numOfPlayers];   //holds the players time since last kill
     private int[] playerPositions = new int[] { 1, 2, 3, 4 };           //holds the players current playerPositions
     private int[] playerDraws = new int[PlayerData.numOfPlayers];
+    private int numOfPlayers = PlayerData.numOfPlayers;
+    private GameObject[] podiumImage = new GameObject[4];
+    private GameObject[] podiumText = new GameObject[4];
 
 
     void Awake()
     {
         //Debug.Log("ScoreboardHandler Awake()");
         //assign variables
-        for (int i = 0; i < playerPositions.Length; i++)
+        for (int i = 0; i < numOfPlayers; i++)
         {
             playerScores[i] = PlayerData.playerScores[i];
             playerTSLKs[i] = PlayerData.timeSinceLastKill[i];
-            Debug.Log("Position " + (i+1) + ": Player " + playerPositions[i] + " with score " + playerScores[i] + " \tTSLK: " + playerTSLKs[i]);
+            //Debug.Log("Position " + (i+1) + ": Player " + playerPositions[i] + " with score " + playerScores[i] + " \tTSLK: " + playerTSLKs[i]);
         }
+
+
+        for (int playerPodPos = 0; playerPodPos < 4; playerPodPos++)
+        {
+            string imgRef = "Image" + (playerPodPos + 1);
+            podiumImage[playerPodPos] = GameObject.Find(imgRef);
+            podiumImage[playerPodPos].SetActive(false);
+
+            string textRef = "Text" + (playerPodPos + 1);
+            podiumText[playerPodPos] = GameObject.Find(textRef);
+            podiumText[playerPodPos].SetActive(false);
+        }
+
 
         SortPlayers();
         DisplayPlayers();
@@ -31,18 +47,19 @@ public class ScoreboardHandler : MonoBehaviour
 
     private void SortPlayers()
     {
-        for(int check = 0; check < playerScores.Length; check++)
+        //for each player in the game
+        for(int check = 0; check < numOfPlayers; check++)
         {
-            for(int player = 0; player < playerScores.Length - check - 1; player++)
+            for(int player = 0; player < numOfPlayers - check - 1; player++)
             {
-                Debug.Log("___________________________");
-                Debug.Log("Player" + player + " v Player" + (player + 1));
-                Debug.Log(playerScores[player] + " v " + playerScores[player + 1]);
+                //Debug.Log("___________________________");
+                //Debug.Log("Player" + player + " v Player" + (player + 1));
+                //Debug.Log(playerScores[player] + " v " + playerScores[player + 1]);
 
                 if(playerScores[player] < playerScores[player+1])
                 {
                     //if current player score is less than next player score, move player down podium
-                    Debug.Log(playerScores[player] + " is less than " + playerScores[player + 1]);
+                    //Debug.Log(playerScores[player] + " is less than " + playerScores[player + 1]);
 
                     //swap scores
                     int tempScore = playerScores[player];
@@ -62,12 +79,12 @@ public class ScoreboardHandler : MonoBehaviour
                 else if(playerScores[player] == playerScores[player + 1])
                 {
                     //if current player score equal to next player score
-                    Debug.Log(playerScores[player] + " is equal to " + playerScores[player + 1]);
+                    //Debug.Log(playerScores[player] + " is equal to " + playerScores[player + 1]);
 
                     if (playerTSLKs[player] > playerTSLKs[player + 1])
                     {
                         //if current player time since last kill greater than next player TSLK, swap podiums
-                        Debug.Log(playerTSLKs[player] + " is greater than " + playerTSLKs[player + 1]);
+                        //Debug.Log(playerTSLKs[player] + " is greater than " + playerTSLKs[player + 1]);
 
                         //swap timers
                         float tempTimer = playerTSLKs[player];
@@ -82,6 +99,11 @@ public class ScoreboardHandler : MonoBehaviour
                 }
             }
         }
+
+        for (int i = 0; i < numOfPlayers; i++)
+        {
+            Debug.Log("Position " + (i+1) + ": Player " + playerPositions[i] + " with score " + playerScores[i] + " \tTSLK: " + playerTSLKs[i]);
+        }
     }
 
 
@@ -90,12 +112,14 @@ public class ScoreboardHandler : MonoBehaviour
         //display winners
         //for each player, set the image in the relevant podium space to the character image
         //then raise the character for each of their kills and add a new podium block below them
-        for (int playerPodPos = 0; playerPodPos < playerScores.Length; playerPodPos++)
+        for (int playerPodPos = 0; playerPodPos < numOfPlayers; playerPodPos++)
         {
             //reference relevant game object associated with scoreboard position i and update image with player sprite in position i 
             string imgRef = "Image" + (playerPodPos + 1);
-            GameObject.Find(imgRef).GetComponent<ChangeImage>().ImageChange(playerSprites[playerPositions[playerPodPos]-1]);
+            podiumImage[playerPodPos].SetActive(true);
+            podiumImage[playerPodPos].GetComponent<ChangeImage>().ImageChange(playerSprites[playerPositions[playerPodPos]-1]);
             Debug.Log("player " + playerPositions[playerPodPos] + " in position " + (playerPodPos + 1));
+            Debug.Log(playerSprites[playerPositions[playerPodPos] - 1]);
 
             //update text
             //reference relevant game object associated with scoreboard position i and update text with the number of kills
@@ -103,16 +127,20 @@ public class ScoreboardHandler : MonoBehaviour
             switch (playerPodPos)
             {
                 case 0:
-                    GameObject.Find(textRef).GetComponent<TMPro.TextMeshProUGUI>().text = "1ST\nKILLS: " + playerScores[playerPodPos];
+                    podiumText[playerPodPos].SetActive(true);
+                    podiumText[playerPodPos].GetComponent<TMPro.TextMeshProUGUI>().text = "1ST\nKILLS: " + playerScores[playerPodPos];
                     break;
                 case 1:
-                    GameObject.Find(textRef).GetComponent<TMPro.TextMeshProUGUI>().text = "2ND\nKILLS: " + playerScores[playerPodPos];
+                    podiumText[playerPodPos].SetActive(true);
+                    podiumText[playerPodPos].GetComponent<TMPro.TextMeshProUGUI>().text = "2ND\nKILLS: " + playerScores[playerPodPos];
                     break;
                 case 2:
-                    GameObject.Find(textRef).GetComponent<TMPro.TextMeshProUGUI>().text = "3RD\nKILLS: " + playerScores[playerPodPos];
+                    podiumText[playerPodPos].SetActive(true);
+                    podiumText[playerPodPos].GetComponent<TMPro.TextMeshProUGUI>().text = "3RD\nKILLS: " + playerScores[playerPodPos];
                     break;
                 case 3:
-                    GameObject.Find(textRef).GetComponent<TMPro.TextMeshProUGUI>().text = "4TH\nKILLS: " + playerScores[playerPodPos];
+                    podiumText[playerPodPos].SetActive(true);
+                    podiumText[playerPodPos].GetComponent<TMPro.TextMeshProUGUI>().text = "4TH\nKILLS: " + playerScores[playerPodPos];
                     break;
             }
         }
