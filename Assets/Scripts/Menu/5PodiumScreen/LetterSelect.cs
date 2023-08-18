b3e0077f473a3de9e15a798050acfe6414a754ed
Atebitss <joseph.c.1998@hotmail.co.uk>
2023-08-18 20:40:03 +0100

    only needs to be connected to egx scoreboard

--- Assets/Scripts/Menu/5PodiumScreen/LetterSelect.cs
+++ Assets/Scripts/Menu/5PodiumScreen/LetterSelect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class LetterSelect : MonoBehaviour
 {
@@ -13,8 +14,9 @@ public class LetterSelect : MonoBehaviour
     [SerializeField] private Text display;  //reference game object text component
     private int charIndex = 0;
     private char[] chars = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-    private char[] adChars = new char[]     //▲alphabet array▲ & ▼dev alphabet array▼
+    private char[] devChars = new char[]     //▲alphabet array▲ & ▼dev alphabet array▼
     {
+              /*letters*/'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                 /*maths*/'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '×', '÷', '%', '√', '∞',
                /*shapes*/'☺', '♥', '♦', '♣', '♠','•','◘','○','◙','♂','♀','♪','♫','☼', '†', 'Þ', ':', ';', 'ˆ', '⌂', '░', '▒', '▓', '■', '▬',
                /*arrows*/'▲', '►', '▼', '◄','↕',
@@ -27,4 +29,30 @@ public class LetterSelect : MonoBehaviour
     };
 
 
+
+    public void LetterInc(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("LetterSelect LetterInc");
+
+        if (PlayerData.GetDevMode() && charIndex < (devChars.Length - 1)){ charIndex++; UpdateDisplay(); }
+        else if (charIndex < (chars.Length - 1)) { charIndex++; UpdateDisplay(); }
+    }
+
+    public void LetterDec(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("LetterSelect LetterDec");
+        if (PlayerData.GetDevMode() && charIndex > 0) { charIndex--; UpdateDisplay(); }
+        else if (charIndex > 0) { charIndex--; UpdateDisplay(); }
+    }
+
+
+    public void WakeDisplay(){ UpdateDisplay(); }
+
+    private void UpdateDisplay()
+    {
+        Debug.Log("LetterSelect UpdateDisplay");
+        Debug.Log("char index: " + charIndex);
+        if (PlayerData.GetDevMode()) { display.text = "" + devChars[charIndex]; }
+        else { display.text = "" + chars[charIndex]; }
+    }
 }
--- Assets/Scripts/Menu/5PodiumScreen/SelectHandler.cs
+++ Assets/Scripts/Menu/5PodiumScreen/SelectHandler.cs
@@ -5,21 +5,42 @@ using UnityEngine.InputSystem;
 
 public class SelectHandler : MonoBehaviour
 {
-    [SerializeField] private LetterSelect[] selectors = new LetterSelect[3];
-    private int selectIndex = 0;
+    private LetterSelect[] selectors = new LetterSelect[3];
+    private int selectorIndex = 0;
 
-    public void OnUpDown(InputAction.CallbackContext ctx)
+
+    void Start()
     {
-
+        Debug.Log("SelectHandler Start");
+        for(int i = 0; i < selectors.Length; i++) { selectors[i] = GameObject.Find("Input" + (i + 1)).GetComponent<LetterSelect>(); selectors[i].WakeDisplay(); }
     }
+
+
+    public void OnUp(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("SelectHandler OnUp");
+        Debug.Log("selector: " + selectors[selectorIndex]);
+
+        if (ctx.started) { selectors[selectorIndex].LetterInc(ctx); }
+    }
+
+    public void OnDown(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("SelectHandler OnDown");
+        Debug.Log("selector: " + selectors[selectorIndex]);
+        if (ctx.started) { selectors[selectorIndex].LetterDec(ctx); }
+    }
+
 
     public void OnConfirm(InputAction.CallbackContext ctx)
     {
-
+        Debug.Log("SelectHandler OnConfirm");
+        if (ctx.started && selectorIndex < (selectors.Length - 1)) { selectorIndex++; }
     }
 
     public void OnBack(InputAction.CallbackContext ctx)
     {
-
+        Debug.Log("SelectHandler OnBack");
+        if (ctx.started && selectorIndex > 0) { selectorIndex--; }
     }
 }
