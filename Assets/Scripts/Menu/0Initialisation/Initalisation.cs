ed8be6e5dac9cab298134f521d15265afd991df1
Atebitss <joseph.c.1998@hotmail.co.uk>
2023-08-23 12:10:12 +0100

    static player sprites

--- Assets/Scripts/Menu/0Initialisation/Initalisation.cs
+++ Assets/Scripts/Menu/0Initialisation/Initalisation.cs
@@ -8,11 +8,15 @@ public class Initalisation : MonoBehaviour
     [SerializeField] private bool devMode;
     [SerializeField] private float devRoundTime = 1;
 
+    //character sprite references
+    [SerializeField] private Sprite[] characterSprites = new Sprite[4];      //holds possible sprites
+
     //instantly loads up the main menu scene, this scene exits to instantiate the audio manager
     void Start()
     {
         PlayerData.SetDevMode(devMode);
         PlayerData.SetDevRoundTime(devRoundTime);
+        PlayerData.SetSprites(characterSprites);
 
         SceneManager.LoadScene(1);
     }    
